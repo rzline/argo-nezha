92a90a50f4184c63fd6d1b004fde42c983b59967
rzline <rzline@proton.me>
2025-03-17 13:03:40 +0800

    fix install.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -295,6 +295,13 @@ autorestart=true
 stderr_logfile=/dev/null
 stdout_logfile=/dev/null
 
+[program:alist]
+command=/dashboard/alist start
+directory=/dashboard
+autostart=true
+autorestart=true
+user=root
+
 [program:aria2]
 command=bash -c "echo 4 | /dashboard/aria2.sh"
 directory=/dashboard
@@ -304,13 +311,6 @@ user=root
 
 [program:sshwifty]
 command=/dashboard/sshwifty
-directory=/dashboard
-autostart=true
-autorestart=true
-user=root
-
-[program:alist]
-command=/dashboard/alist start
 directory=/dashboard
 autostart=true
 autorestart=true

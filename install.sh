82b914123c79774e8f2e307998599c0000d8ff0a
rzline <rzline@proton.me>
2025-03-17 12:24:00 +0800

    fix install.sh

--- install.sh
+++ install.sh
@@ -9,13 +9,12 @@ chmod +x aria2.sh
 echo 1 | ./aria2.sh
 echo 11 | ./aria2.sh
 echo -e "7\n1\n123456" | ./aria2.sh
-wget -q https://github.com/AlistGo/alist/releases/latest/download/alist-linux-amd64.tar.gz
+wget https://github.com/AlistGo/alist/releases/latest/download/alist-linux-amd64.tar.gz
 tar -zxvf alist-linux-amd64.tar.gz
 chmod +x alist
 ./alist admin set 123456
 rm alist-linux-amd64.tar.gz
-mkdir /tmp
-wget -qP /tmp/ https://github.com/nirui/sshwifty/releases/download/0.3.19-beta-release-prebuild/sshwifty_0.3.19-beta-release_linux_amd64.tar.gz
+wget -P /tmp/ https://github.com/nirui/sshwifty/releases/download/0.3.19-beta-release-prebuild/sshwifty_0.3.19-beta-release_linux_amd64.tar.gz
 tar -zxvf /tmp/sshwifty_0.3.19-beta-release_linux_amd64.tar.gz /tmp
 mv /tmp/sshwifty_linux_amd64 /dashboard/sshwifty
 chmod +x sshwifty
